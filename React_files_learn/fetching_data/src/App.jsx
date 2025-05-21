0abab6fa27ebf5d9c11b74a5b3173b187322ff86
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-05-21 19:41:36 +0600

    API work exersise -2

--- React_files_learn/fetching_data/src/App.jsx
+++ React_files_learn/fetching_data/src/App.jsx
@@ -1,15 +1,23 @@
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
 import { getPosts } from './api';
+import PostCard from './PostCard';
 import "./App.css";
+
 function App() {  
+
+  const  [data , setData] = useState(null); 
+
 useEffect(() =>{ 
 
-  getPosts().then((posts)=> console.log(posts));
+  getPosts().then((posts)=> setData(posts));
 }, []);
-return <div className="App">Api Int.</div>
+return (
+<div className="App">
+{
+  data ? data.map((e) => <PostCard /> ) : <p> NO data</p>
+}
 
-
+</div>
+); 
 } 
-
-
 export default App;
