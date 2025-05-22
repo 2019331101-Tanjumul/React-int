6e70fd08985081bfb7170034336d8d6992a43ade
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-05-22 17:06:58 +0600

    submit button ready

--- React_files_learn/CRUD/CRUD/src/Users/index.jsx
+++ React_files_learn/CRUD/CRUD/src/Users/index.jsx
@@ -13,9 +13,12 @@ const User = () => {
 
        
           <input type="number" name="number" id="number" placeholder = " Enter your age" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+          <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
         </div>
 
-
+  
 
        
     </form>
