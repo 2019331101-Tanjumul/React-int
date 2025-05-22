dade88e9a94a376beafb4890edc1f2196fa50755
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-05-22 17:00:31 +0600

    username, email & age are fine

--- React_files_learn/CRUD/CRUD/src/Users/index.jsx
+++ React_files_learn/CRUD/CRUD/src/Users/index.jsx
@@ -2,10 +2,22 @@ const User = () => {
     return (
       
   <div>
-    <form>
-        <input type= "text" name = "name" />
-        <input type = "text" name = "email"/>
-        <input type = "text" name = "email"/>
+    <form className=" flex flex-center justify-center">
+    <div class="mt-2 flex flex-col gap-2 w-[40%]">
+          <input type="text" name="username" id="username" placeholder = " Enter your short name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+       
+
+        
+          <input type="email" name="email" id="email" placeholder = " Enter your email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+       
+
+       
+          <input type="number" name="number" id="number" placeholder = " Enter your age" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+        </div>
+
+
+
+       
     </form>
   </div>
     );
