61482f2501d507fc7269874599159fb3e0ff3e15
2019331101-Tanjumul <crackerroot4@gmail.com>
2025-05-22 12:45:30 +0600

    changed the width of the box in tailwind css for the form

--- React_files_learn/CRUD/CRUD/src/Users/index.jsx
+++ React_files_learn/CRUD/CRUD/src/Users/index.jsx
@@ -5,7 +5,7 @@ const User = () => {
      <div>
         <form action="">
 
-       <div className="mt-5 flex flex-col gap-2">
+       <div className="mt-5 flex flex-col gap-2 w-[50%]">
        <input type="name " name="name" id="name" autocomplete="" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
         
         <input type="email" name="email" id="email" autocomplete="" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
--- React_files_learn/fetching_data/src/api/index.js
+++ React_files_learn/fetching_data/src/api/index.js
@@ -4,3 +4,17 @@ const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
  }); 
 return await response.json(); 
 }; 
+
+
+/* 
+ const getPosts = async ()=> { 
+    
+    const response = await fetch('https://torro.com/api/v1/posts',{ 
+    method : 'GET', 
+    
+});
+    return await response.json(); 
+}; 
+*/
+
+
